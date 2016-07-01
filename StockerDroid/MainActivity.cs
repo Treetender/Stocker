f5f5e1a72523787920ea4235d7952e98203f806f
Treetender <treetender@me.com>
2016-07-01 00:09:21 -0400

    Attempted to make the android button responsive

--- StockerDroid/MainActivity.cs
+++ StockerDroid/MainActivity.cs
@@ -5,8 +5,8 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
-using System.Collections.Generic;
 using StockerCore.DL;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using StockerCore.SAL;
 
@@ -16,43 +16,59 @@ namespace StockerDroid
     public class MainActivity : Activity
     {
         private List<Stock> mStocks;
-        private ProgressBar mBar;
+        private Button mButton;
+        private bool mIsFetching = false;
 
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
 
+            mStocks = new List<Stock>();
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
 
             // Get our button from the layout resource,
             // and attach an event to it
-            Button button = FindViewById<Button>(Resource.Id.MyButton);
-            mBar = FindViewById<ProgressBar>(Resource.Id.progressBar1);
+            if(mButton == null)
+                mButton = FindViewById<Button>(Resource.Id.MyButton);
 
-            button.Click += delegate
-            {
-                Task.Run(delegate { mStocks = GetStocks().Result; }).Wait();
+            mButton.Click += delegate 
+            {         
+                if (!mIsFetching) 
+                {
+                    mIsFetching = true;
+                    mButton.Text = "Downloading Stocks...";
+                    var fetcher = new StockFetcher();
+                    fetcher.OnDownloadComplete += (sender, e) => { mButton.Text = "Download Complete"; Console.WriteLine("Download Complete"); };
+                    fetcher.OnProgressChanged += (sender, progress) => { mButton.Text = string.Format("{0:p} Complete", progress); Console.WriteLine(string.Format("{0:p}")); };
+                    Task.Run(() => { mStocks = fetcher.GetStocks().Result; });
+                }
             };
-        }
+       }
 
-        private async Task<List<Stock>> GetStocks()
-        {
-            var http = new StockHTTP();
-            var queries = await http.DownloadCompaniesFromCSVAsync();
+       private class StockFetcher 
+       {
+            public event EventHandler<decimal> OnProgressChanged;
+            public event EventHandler OnDownloadComplete; 
 
-            List<Stock> stocks = new List<Stock>();
-            decimal i = 1;
-            decimal totals = queries.Count;
-
-            foreach (string query in queries)
+            public async Task<List<Stock>> GetStocks()
             {
-                List<Stock> results = await http.DownloadStocksFromYQL(query);
-                stocks.AddRange(results);
-                mBar.Progress = (int)(i / totals);
+                var http = new StockHTTP();
+                var queries = await http.DownloadCompaniesFromCSVAsync();
+                List<Stock> stocks = new List<Stock>();
+                decimal i = 1.0M;
+                decimal totals = queries.Count;
+                foreach (var query in queries)
+                {
+                    List<Stock> results = await http.DownloadStocksFromYQL(query);
+                    results.ForEach(r => stocks.Add(r));
+                    OnProgressChanged?.Invoke(this, (i / totals));
+                    i += 1;
+                }
+                OnProgressChanged?.Invoke(this, 1);
+                OnDownloadComplete?.Invoke(this, EventArgs.Empty);
+                return stocks;
             }
-
-            return stocks;
         }
     }
 }
